0239521c240de76a3349ee3cca22926db010ee00
PyotrWrangel <maerlyn@hotmail.it>
2025-09-19 19:16:23 +0200

    migliorato layout

--- src/Pages/HomePage/components/HeroImage.jsx
+++ src/Pages/HomePage/components/HeroImage.jsx
@@ -1,22 +1,32 @@
 function HeroImage({ title, description = [] }) {
   return (
     <>
-      <div className="lg:w-full grid lg:grid-cols-2">
-        <article className="bg-green-600  h-100 py-10 px-5">
-          <h2 className="font-bold text-left text-2xl text-white mt-9 mb-9">{title}</h2>
+      <div className="bg-green-600 lg:w-full grid lg:grid-cols-2 mt-[100px]">
+        <article className=" xl:w-full   py-16 px-5">
+          <h2 className="font-bold text-left text-3xl text-white mb-8">{title}</h2>
           {/* mapping dentro il componente*/}
           {description.map((desc, index) => (
             // passiamo index che è l'id e desc che è l'array delle descrizioni
-            <p key={index} className="text-2xl text-left text-white">{desc}</p>
+            <p key={index} className="text-xl text-left text-white mb-3">{desc}</p>
           ))}   
         </article>
+         <div className="flex justify-end items-center">
         <img
           src="/images/Specialization.jpg"
-          className=" w-300 h-100"
+          className="flex w-full xl:max-w-[700px] xl:flex-row xl:justify-end h-[400px]"
         />
+        </div>
       </div>
     </>
   );
 }
 
 export default HeroImage;
+
+
+
+// sm: → ≥ 640px
+// md: → ≥ 768px
+// lg: → ≥ 1024px
+// xl: → ≥ 1280px
+// 2xl: → ≥ 1536px
