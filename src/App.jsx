fb3ee4af06d93115438a8fc97d8ab4f95611396b
PyotrWrangel <maerlyn@hotmail.it>
2025-09-22 11:36:57 +0200

    aggiunto redirect su componente Working.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import Homepage from './Pages/HomePage/Homepage'
+import About from './Pages/aboutUsPage/About'
+import Register from './Pages/RegisterPage/Register'
+import Insurance from './Pages/InsurancePage/Insurance'
+import Services from './Pages/ServicesPage/Service'
+import Specialists from './Pages/SpecialistsPage/Specialists'
+import Working from './assets/workingAdvisor/working'
+
+
+
 
 function App() {
 
   return (
     <>
-      <Homepage></Homepage>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<Working title="Pagina about in costruzione"/>} />
+        <Route path="/insurance" element={<Working title="Pagina assicurazioni in lavorazione" />} />
+        <Route path="/services" element={<Working title="Pagina servizi in lavorazione" />} />
+        <Route path="/specialists" element={<Working title="Pagina specialisti in lavorazione" />} /> 
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
